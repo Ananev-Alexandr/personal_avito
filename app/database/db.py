ceddaecb373e3c08d35ecb06e50e801aac814a9f
Ananev-Alexandr <ananevalexandr@mail.ru>
2023-03-13 21:14:11 +0300

    [BF] #Change URL

--- app/database/db.py
+++ app/database/db.py
@@ -4,13 +4,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = f"postgresql:\
-//{os.getenv('POSTGRES_USER')}\
-:{os.getenv('POSTGRES_PASSWORD')}@"\
-f"{os.getenv('POSTGRES_HOST')}:\
-5432/{os.getenv('POSTGRES_DB')}"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+URL = os.getenv('SQLALCHEMY_DATABASE_URL')
+engine = create_engine(URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
