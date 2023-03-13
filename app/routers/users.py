9351d53df1aa888fbac18b0c458c77eb43928d66
Ananev-Alexandr <ananevalexandr@mail.ru>
2023-03-13 21:57:29 +0300

    [NF] #Add router security with token

--- app/routers/users.py
+++ app/routers/users.py
@@ -5,18 +5,42 @@ from app.database.db_connect import get_db
 from app.database import crud
 from app.schemas import user_schemas
 from app.database.db import Base, engine
-
-Base.metadata.create_all(bind=engine)
+from app.security import services
+from app.security.schemas import Token
+from fastapi.security import OAuth2PasswordRequestForm
+from datetime import timedelta
+import os
 
 
 router = APIRouter(tags=["users"])
 
 
 @router.get("/")
-async def hello() -> dict:
+async def hello(security=Depends(services.get_current_user)) -> dict:
     return {"Hello": "World"}
 
 
 @router.post("/users/", response_model=user_schemas.UserOut)
 async def create_user(user: user_schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
+
+
+@router.post("/token", response_model=Token, include_in_schema=False)
+async def login_for_access_token(
+    db: Session = Depends(get_db),
+    form_data: OAuth2PasswordRequestForm = Depends()
+        ):
+    user = services.login(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(
+        minutes=int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+        )
+    access_token = services.create_access_token(
+        data={"sub": user.login}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
