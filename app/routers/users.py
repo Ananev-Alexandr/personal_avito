87e4deb4861629bc3a7df88e05bae84fd484c4c6
Ananev-Alexandr <ananevalexandr@mail.ru>
2023-03-13 20:59:11 +0300

    [NF] #Add router to create user

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,4 +1,13 @@
 from fastapi import APIRouter
+from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, HTTPException, status
+from app.database.db_connect import get_db
+from app.database import crud
+from app.schemas import user_schemas
+from app.database.db import Base, engine
+
+Base.metadata.create_all(bind=engine)
+
 
 router = APIRouter(tags=["users"])
 
@@ -6,3 +15,8 @@ router = APIRouter(tags=["users"])
 @router.get("/")
 async def hello() -> dict:
     return {"Hello": "World"}
+
+
+@router.post("/users/", response_model=user_schemas.UserOut)
+async def create_user(user: user_schemas.UserCreate, db: Session = Depends(get_db)):
+    return crud.create_user(db=db, user=user)
