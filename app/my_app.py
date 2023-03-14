4f9584db00dea409a624279391f76cba96df6654
Ananev-Alexandr <ananevalexandr@mail.ru>
2023-03-14 11:13:58 +0300

    [NF] #Included adv_router

--- app/my_app.py
+++ app/my_app.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
 from app.routers.users import router as user_router
+from app.routers.advertisements import router as adv_router
 
 app = FastAPI()
 
 
 app.include_router(user_router)
+app.include_router(adv_router)
