58bfefc47bba877a7f5e5cad6413fa2184862b14
Ananev-Alexandr <ananevalexandr@mail.ru>
2023-03-14 14:10:52 +0300

    [NF] #Create schemas feedback

--- app/schemas/adv_schemas.py
+++ app/schemas/adv_schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from datetime import datetime
+from fastapi import Path
 
 
 class AdvIn(BaseModel):
@@ -10,5 +11,18 @@ class AdvDB(AdvIn):
     id: int
     date_of_publication: datetime
 
+    class Config:
+        orm_mode = True
+
+
+class FeedbackIn(BaseModel):
+    advertisement_id: int
+    message: str
+    rate: int = Path(title="int",gt=1,le=10)
+    
+class FeedbackDB(FeedbackIn):
+    id: int
+    user_id: int
+    
     class Config:
         orm_mode = True
