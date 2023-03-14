7f53ad8ed3c45d7720a76f636a6078c946f91b66
Ananev-Alexandr <ananevalexandr@mail.ru>
2023-03-14 18:37:02 +0300

    [NF] #Added access for active users and admins

--- app/database/crud.py
+++ app/database/crud.py
@@ -8,7 +8,6 @@ from app.database import models
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-
 def verify_password(plain_password, hashed_password) -> bool:
     eq_pass = pwd_context.verify(plain_password, hashed_password)
     return eq_pass
@@ -51,70 +50,94 @@ def info_about_user_for_login(db: Session, login: str):
         return db_user
     
 
-def create_advertisement(db: Session, adv: adv_schemas.AdvIn, user_id: int):
-    db_adv = models.Advertisements(
-        user_id=user_id,
-        content=adv.content,
-    )
-    db.add(db_adv)
-    db.commit()
-    db.refresh(db_adv)
-    return db_adv
+def create_advertisement(db: Session, adv: adv_schemas.AdvIn, user_id: int, current_user):
+    if current_user.active:
+        db_adv = models.Advertisements(
+            user_id=user_id,
+            content=adv.content,
+        )
+        db.add(db_adv)
+        db.commit()
+        db.refresh(db_adv)
+        return db_adv
+    else:
+        raise HTTPException(
+            status_code=404,
+            detail="Access Denied, You Don’t Have Permission"
+                )
 
 
-def get_all_adv(db: Session):
-    get_adv = db.query(models.Advertisements).all()
-    return get_adv
-
-
-def info_about_adv(db: Session, id: int):
-    get_adv = db.query(models.Advertisements).\
-        filter(models.Advertisements.id == id).one_or_none()
-    if get_adv:
+def get_all_adv(db: Session, current_user):
+    if current_user.active:
+        get_adv = db.query(models.Advertisements).all()
         return get_adv
-    raise HTTPException(status_code=404, detail="Id not found")
-
-
-def delete_adv(id: int, user_id: int, db: Session):
-    find_adv = db.query(models.Advertisements).\
-        filter(
-            models.Advertisements.id == id,
-            models.Advertisements.user_id == user_id
-                ).one_or_none()
-    if find_adv:
-        adv = db.query(models.Advertisements).filter(models.Advertisements.id == id).first()
-        db.delete(adv)
-        db.commit()
-        return {"message": "Success delete!"}
     else:
         raise HTTPException(
-            status_code=403,
-            detail="Its not your post, post not found"
+            status_code=404,
+            detail="Access Denied, You Don’t Have Permission"
                 )
-        
-def post_a_feedback(feedback, user_id: int, db: Session):
-    find_feedback = db.query(models.Feedback).\
-        filter(models.Feedback.advertisement_id == feedback.advertisement_id).\
-            filter(models.Feedback.user_id == user_id).one_or_none()
-    if not find_feedback:
-        fb = models.Feedback(
-        user_id=user_id,
-        message=feedback.message,
-        rate=feedback.rate,
-        advertisement_id=feedback.advertisement_id
-    )
-        db.add(fb)
-        db.commit()
-        return {"message": "Success!"}
+
+
+def info_about_adv(db: Session, id: int, current_user):
+    if current_user.active:
+        get_adv = db.query(models.Advertisements).\
+            filter(models.Advertisements.id == id).one_or_none()
+        if get_adv:
+            return get_adv
+        raise HTTPException(status_code=404, detail="Id not found")
     else:
         raise HTTPException(
-            status_code=403,
-            detail="You have already left a feedback"
+            status_code=404,
+            detail="Access Denied, You Don’t Have Permission"
+                )
+
+def delete_adv(id: int, user_id: int, db: Session, current_user):
+    if current_user.active:
+        find_adv = db.query(models.Advertisements).\
+            filter(
+                models.Advertisements.id == id,
+                models.Advertisements.user_id == user_id
+                    ).one_or_none()
+        if find_adv:
+            adv = db.query(models.Advertisements).filter(models.Advertisements.id == id).first()
+            db.delete(adv)
+            db.commit()
+            return {"message": "Success delete!"}
+        else:
+            raise HTTPException(
+                status_code=403,
+                detail="Its not your post, post not found"
+                    )
+    else:
+        raise HTTPException(
+            status_code=404,
+            detail="Access Denied, You Don’t Have Permission"
                 )
         
-def ban(id: int, db: Session, now_user_id: int):
-    find_root = db.query(models.User).filter(models.User.id == now_user_id).filter(models.User.role_id == 2).one_or_none()
-    if find_root:
+        
+def post_a_feedback(feedback, user_id: int, db: Session, current_user):
+    if current_user.active:
+        find_feedback = db.query(models.Feedback).\
+            filter(models.Feedback.advertisement_id == feedback.advertisement_id).\
+                filter(models.Feedback.user_id == user_id).one_or_none()
+        if not find_feedback:
+            fb = models.Feedback(
+            user_id=user_id,
+            message=feedback.message,
+            rate=feedback.rate,
+            advertisement_id=feedback.advertisement_id
+        )
+            db.add(fb)
+            db.commit()
+            return {"message": "Success!"}
+        else:
+            raise HTTPException(
+                status_code=403,
+                detail="You have already left a feedback"
+                    )
+        
+def ban(id: int, db: Session, current_user):
+    if current_user.role_id == 2:
         interesting_user = db.query(models.User).filter(models.User.id == id)
         if interesting_user.filter(models.User.active == True).one_or_none():
             interesting_user = interesting_user.update({models.User.active: False})
--- app/routers/advertisements.py
+++ app/routers/advertisements.py
@@ -13,45 +13,45 @@ router = APIRouter(tags=["Advertisements"])
 async def create_advertisements(
     adv: adv_schemas.AdvIn,
     db: Session = Depends(get_db),
-    security=Depends(services.get_current_user)
+    current_user=Depends(services.get_current_user)
         ):
-    return crud.create_advertisement(db=db, adv=adv, user_id=security.id)
+    return crud.create_advertisement(db=db, adv=adv, user_id=current_user.id, current_user=current_user)
 
 
 @router.post("/all_advertisements/")
 async def all_advertisements(
     db: Session = Depends(get_db),
-    security=Depends(services.get_current_user)
+    current_user=Depends(services.get_current_user)
         ):
-    return crud.get_all_adv(db=db)
+    return crud.get_all_adv(db=db, current_user=current_user)
 
 
 @router.get("/advertisements/{id}/", response_model=adv_schemas.AdvDB)
 async def info_about_adv(
     id: int,
     db: Session = Depends(get_db),
-    security=Depends(services.get_current_user)
+    current_user=Depends(services.get_current_user)
         ):
-    return crud.info_about_adv(id=id, db=db)
+    return crud.info_about_adv(id=id, db=db, current_user=current_user)
 
 
 @router.delete("/advertisements/{id}/")
 async def delete_advt(
     id: int,
     db: Session = Depends(get_db),
-    security=Depends(services.get_current_user)
+    current_user=Depends(services.get_current_user)
         ):
-    return crud.delete_adv(id=id, user_id=security.id, db=db)
+    return crud.delete_adv(id=id, user_id=current_user.id, db=db, current_user=current_user)
 
 
 @router.post("/feedback/")
 async def post_a_feedback(
     fb: adv_schemas.FeedbackIn,
     db: Session = Depends(get_db),
-    security=Depends(services.get_current_user)
+    current_user=Depends(services.get_current_user)
         ):
     return crud.post_a_feedback(
         feedback=fb,
-        user_id=security.id,
+        user_id=current_user.id,
         db=db
         )
