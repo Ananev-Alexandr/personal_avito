9c635517415b919cce6d4bc77df266795f0ccb9c
Ananev-Alexandr <ananevalexandr@mail.ru>
2023-03-14 21:04:39 +0300

    [BF] #Rename file

---
+++ app/app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+from app.application_objects.users.router import router as user_router
+from app.application_objects.advertisements.router import router as adv_router
+from app.application_objects.token.router import router as token_router
+
+
+app = FastAPI()
+
+
+app.include_router(user_router)
+app.include_router(adv_router)
+app.include_router(token_router)
+
