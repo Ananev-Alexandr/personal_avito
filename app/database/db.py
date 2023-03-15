008ee22d8513cd84b0ca2ea2bdc737d672e4804a
Ananev-Alexandr <ananevalexandr@mail.ru>
2023-03-15 12:08:49 +0300

    [BF] #Changed data loading from the environment

--- app/database/db.py
+++ app/database/db.py
@@ -3,10 +3,12 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-URL = os.getenv('SQLALCHEMY_DATABASE_URL')
-engine = create_engine(URL)
+engine = create_engine(os.getenv('SQLALCHEMY_DATABASE_URL'))
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
