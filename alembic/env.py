354473af901807f4929869b7ca65147bc98c4a85
Ananev-Alexandr <ananevalexandr@mail.ru>
2023-03-15 23:22:20 +0300

    [NF] #Change table complaint

--- alembic/env.py
+++ alembic/env.py
@@ -1,11 +1,10 @@
+import os
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-import os
+from dotenv import load_dotenv
+from sqlalchemy import engine_from_config, pool
 
 from alembic import context
-from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -25,6 +24,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 from app.database.db import Base
+
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
