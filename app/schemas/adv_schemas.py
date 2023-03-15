a27b4e26cf56348bd6e279507b8ed46b85a8ac12
Ananev-Alexandr <ananevalexandr@mail.ru>
2023-03-15 15:35:15 +0300

    [NF] #Added new schemas to filter and sort adv

--- app/schemas/adv_schemas.py
+++ app/schemas/adv_schemas.py
@@ -1,16 +1,26 @@
 from pydantic import BaseModel
-from datetime import datetime
+from datetime import datetime, date
 from fastapi import Path
+from typing import Union
 
 
 class AdvIn(BaseModel):
     content: str
     group_id: int
-
+    title: str
 
 class AdvDB(AdvIn):
     id: int
     date_of_publication: datetime
+
+    class Config:
+        orm_mode = True
+        
+
+class FilterAdv(BaseModel):
+    content: Union[str, None] = None
+    group_id: Union[int, None] = 1
+    title: Union[str, None] = None
 
     class Config:
         orm_mode = True
@@ -20,10 +30,26 @@ class FeedbackIn(BaseModel):
     advertisement_id: int
     message: str
     rate: int = Path(title="int",gt=1,le=10)
-    
+
+
 class FeedbackDB(FeedbackIn):
     id: int
     user_id: int
     
     class Config:
         orm_mode = True
+        
+
+class DateFilter(BaseModel):
+    start_date: Union[date, None] = None
+    end_date: Union[date, None] = None
+
+
+class SortAdv(BaseModel):
+    group_by: Union[str, None] = "asc"
+    sort_by: Union[str, None] = "date_of_publication"
+
+
+class FilterAndSortAdv(BaseModel):
+    filters: Union[FilterAdv, None] = None
+    group: Union[SortAdv, None] = None
